40e393c83ca89c327ca864561d1155566e35e735
trustyourse1f <kissday98@nate.com>
2022-04-14 22:41:43 +0900

    색종이

--- Algorithm/BOJ_2563.py
+++ Algorithm/BOJ_2563.py
@@ -1,2 +1,18 @@
-paper_num=int(input())
-color_papers=[list(int(input().split(" "))) for _ in range(paper_num)]
+도화지 = [list([0]*100) for _ in range(100)]
+
+색종이의_수 = int( input() )
+
+for _ in range(색종이의_수):
+    x,y = map(int, (input().split(" ")))
+    for i in range(100-(y+10), 100-y):
+        for j in range(x, x+10):
+            도화지[i][j] = 1
+
+count = 0
+for i in range(100):
+    for j in range(100):
+        if 도화지[i][j] == 1:
+            count+=1
+
+
+print(count)
