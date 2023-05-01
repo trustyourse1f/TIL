a770b5ba6fdfca67d862087508d6254fecbf62da
trustyourse1f <kissday98@nate.com>
2023-05-01 21:20:26 +0900

    실습 추가

---
+++ Data_Visualization/Numpy/day1/Ex1.py
@@ -0,0 +1,15 @@
+l = [1, 2, 3, 4]
+l1 = [1, 2, 3]
+l2 = [1, 2]
+al = [l, l1, l2]
+l + l1 #연결
+
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+a1 = np.array([1, 2, 3, 4])
+print(a + a1)
+
+a2 = np.array([1, 2, 3, 4])
+l = [[1, 2, 3, 4], [1, 2, 3, 4]]
+np.random.randint(1, 100, 100)
